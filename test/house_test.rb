73c3399c80d044732ebb88333b5dc77f57a09bf9
Paul Debevec <paulmdebevec@gmail.com>
2019-10-09 10:24:57 -0600

    Added test_rooms_from_room_type_category method

--- test/house_test.rb
+++ test/house_test.rb
@@ -9,6 +9,8 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, 13)
     @room_2 = Room.new(:bedroom, 11, 15)
+    @room_3 = Room.new(:living_room, 25, 15)
+    @room_4 = Room.new(:basement, 30, 41)
   end
 
   def test_does_the_house_exist
@@ -16,27 +18,35 @@ class HouseTest < Minitest::Test
   end
 
   def test_does_the_house_have_a_price
-    skip
     assert_equal 400000, @house.price
   end
 
   def test_does_the_house_have_an_address
-    skip
     assert_equal "123 sugar lane", @house.address
   end
 
   def test_what_rooms_does_the_house_have
-    skip
     assert_equal [], @house.rooms
   end
 
   def test_add_room_to_house
-    skip
     assert_equal [], @house.rooms
-    house.add_room(@room_1)
-    assert_equal @rooms, [@room_1]
-    house.add_room(@room_2)
-    assert_equal @rooms, [@room_1, @room_2]
+
+    @house.add_room(@room_1)
+    assert_equal [@room_1], @house.rooms
+
+    @house.add_room(@room_2)
+    assert_equal [@room_1, @room_2], @house.rooms
+  end
+
+  def test_rooms_from_room_type_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+    assert_equal [], @house.rooms_from_category(:backyard)
   end
 
 end
